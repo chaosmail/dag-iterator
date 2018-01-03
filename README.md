5ecfd507a6a09f9e817fcfc6772caabd2222668c
Christoph Koerner <office@chaosmail.at>
2018-01-03 20:40:59 +0100

    add tests

--- README.md
+++ README.md
@@ -2,6 +2,39 @@
 
 An utility tool to traverse DAG graphs in JavaScript by depth-first search. It automatically injects the previous nodes as arguments for the current node. This can be used in Deep Learning to traverse the model graph of a DNN such as GoogLeNet.
 
+## Usage Typescript
+
+```ts
+import * as dag from 'dag-iterator';
+
+const nodes: dag.INode<String>[] = [
+  {name: "A", data: "Node A"},
+  {name: "B", data: "Node B"},
+  {name: "C", data: "Node C"},
+  {name: "D", data: "Node D"}
+];
+
+const edges: dag.IEdge[] = [
+  {src: "A", dst: "B"},
+  {src: "A", dst: "C"},
+  {src: "B", dst: "C"},
+  {src: "C", dst: "D"}
+];
+
+dag.iterate<String>(nodes, edges, (node, parents, i) => {
+  console.log(node, parents, i);
+});
+
+/*
+ * Outputs:
+ * > Node A [] 0
+ * > Node B ["Node A"] 1
+ * > Node C (2) ["Node A", "Node B"] 2
+ * > Node D ["Node C"] 3
+ */
+
+```
+
 ## Usage Browser
 
 ```html
@@ -24,7 +57,7 @@ An utility tool to traverse DAG graphs in JavaScript by depth-first search. It a
 
   // Iterate the graph
   dagIterator.iterate(nodes, edges, function(node, parents, i){
-    console.log(node, parents, i)
+    console.log(node, parents, i);
   });
 
   /*
---
+++ test/dag.spec.ts
@@ -0,0 +1,86 @@
+import {INode, IEdge, iterate} from '../src/index';
+
+describe('dagIterator::iterateDfs', () => {
+
+  it('iterate', () => {
+    
+    const nodes: INode<String>[] = [
+      {name: "A", data: "Node A"},
+      {name: "B", data: "Node B"},
+      {name: "C", data: "Node C"},
+      {name: "D", data: "Node D"}
+    ];
+
+    const edges: IEdge[] = [
+      {src: "A", dst: "B"},
+      {src: "A", dst: "C"},
+      {src: "B", dst: "C"},
+      {src: "C", dst: "D"}
+    ];
+
+    var j = 0;
+
+    iterate<String>(nodes, edges, (node, parents, i) => {
+      
+      switch(j) {
+        case 0:
+          expect(node).toEqual(nodes[0].data);
+          expect(parents).toEqual([]);
+          break;
+        case 1:
+          expect(node).toEqual(nodes[1].data);
+          expect(parents).toEqual([nodes[0].data]);
+          break;
+        case 2:
+          expect(node).toEqual(nodes[2].data);
+          expect(parents).toEqual([nodes[0].data, nodes[1].data]);
+          break;
+        case 3:
+          expect(node).toEqual(nodes[3].data);
+          expect(parents).toEqual([nodes[2].data]);
+          break;
+        default:
+          expect(false).toEqual(true);
+      }
+
+      j += 1;
+    });
+  });
+
+  it('iterateUntil', () => {
+    
+    const nodes = [
+      {name: "A", data: "Node A"},
+      {name: "B", data: "Node B"},
+      {name: "C", data: "Node C"},
+      {name: "D", data: "Node D"}
+    ];
+
+    const edges = [
+      {src: "A", dst: "B"},
+      {src: "A", dst: "C"},
+      {src: "B", dst: "C"},
+      {src: "C", dst: "D"}
+    ];
+
+    var j = 0;
+
+    iterate<String>(nodes, edges, (node, parents, i) => {
+      
+      switch(j) {
+        case 0:
+          expect(node).toEqual(nodes[0].data);
+          expect(parents).toEqual([]);
+          break;
+        case 1:
+          expect(node).toEqual(nodes[1].data);
+          expect(parents).toEqual([nodes[0].data]);
+          break;
+        default:
+          expect(false).toEqual(true);
+      }
+
+      j += 1;
+    }, nodes[1].name);
+  });
+});
